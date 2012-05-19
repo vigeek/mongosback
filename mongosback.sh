6a8bf2fe4fd54857e3283fe996c02cd7f9c9838f
Russ Thompson <vigeek>
2012-05-19 01:11:16 -0400

    ftp and scp exporting, archiving and compression added

--- mongosback.sh
+++ mongosback.sh
@@ -6,7 +6,7 @@
 
 if [ -f "mongosback.conf" ] ; then
 	. ./mongosback.conf
-   COMPRESSED_NAME="$(DO_BACKUP)_$(date +%m_%d_%Y)_dump.tar.gz"
+   COMPRESSED_NAME="$(DO_BACKUP)_$(date +%m_%d_%Y)_dump.tar"
 else
 	logger "mongosback unable to read the configuration file, exiting prematurely"
 	exit 1
@@ -50,55 +50,92 @@ function prepare_job {
   fi
 }
 
-function archive {
-
+function compress {
   if [ $COMPRESS -eq "1" ] ; then
-    if [ -d "$BACKUP_PATH/$DO_BACKUP" ] ; then
+
+    if [ -d "$COMPRESSED_NAME" ] ; then
       log "compressing backup" >> $LOG_FILE
-      GZIP=$COMPRESSION_LEVEL ; tar -czvf $COMPRESSED_NAME $BACKUP_PATH/$DO_BACKUP
+      gzip $COMPRESSION_LEVEL $COMPRESSED_NAME
+      COMPRESSED_NAME="$COMPRESSED_NAME.gz"
     else
       log "ERROR-> $DO_BACKUP does not exst."
       exit 1
     fi
   fi
+}
 
+function archive {
 
   if [ $DAYS_TO_KEEP -gt "1" ] ; then
-    TO_DELETE=`find $COMPRESS_STORE -name "dump.tar" -type f -mtime +$DAYS_TO_KEEP`
+    TO_DELETE=`find $COMPRESS_STORE -name "*dump.tar*" -type f -mtime +$DAYS_TO_KEEP`
       if [ ! -z "$TO_DELETE" ] ; then
         log "deleting files older than $DAYS_TO_KEEP days:"
-        find $COMPRESS_STORE -name "dump.tar" -type f -mtime +$DAYS_TO_KEEP -exec du -hs {} \; >> $LOG_FILE
-        find $COMPRESS_STORE -name "dump.tar" -type f -mtime +$DAYS_TO_KEEP -exec rm {} \;
+        find $COMPRESS_STORE -name "*dump.tar*" -type f -mtime +$DAYS_TO_KEEP -exec du -hs {} \; >> $LOG_FILE
+        find $COMPRESS_STORE -name "*dump.tar*" -type f -mtime +$DAYS_TO_KEEP -exec rm {} \;
       fi
   else
     find $COMPRESS_STORE -name "dump.tar" -exec rm {} \;
   fi
 }
+
+function perform_backup {
+
+  cd $BACKUP_PATH
+  if [ -z "$DO_BACKUP" ] ; then
+    cd $BACKUP_PATH
+    log "$(date) starting mongodump (this will take a while)...." 
+    $PERFORMANCE_THROTTLING $MONGO_DUMP -h $MONGO_HOST_PORT $MONGO_DUMP_OPTIONS
+    if [ $? -ne 0 ] ; then
+      log "ERROR -> mongo_dump failed to execute properly: err $?"
+      log "mongosback terminating early" 
+      exit 1
+    fi
+    tar --remove-files -cf $COMPRESSED_NAME dump/
+  else
+    log "$(date) starting mongodump (this will take a while)...." 
+    $PERFORMANCE_THROTTLING $MONGO_DUMP -h $MONGO_HOST_PORT -d $DO_BACKUP -o $BACKUP_PATH $MONGO_DUMP_OPTIONS
+    if [ $? -ne 0 ] ; then
+      log "ERROR -> mongo_dump failed to execute properly: err $?"
+      log "mongosback terminating early" 
+      exit 1
+    fi
+    tar --remove-files -cf $COMPRESSED_NAME dump/
+  fi
+  log "mongodump successful, beginning post-dump operations" 
+}
+
+function ftp_export {
+
+  if [ $FTP_EXPORT == "1" ] ; then
+    ftp -n $FTP_HOST << EOF
+      quote USER "$FTP_USER"
+      quote PASS "$FTP_PASS"
+      cd $FTP_PATH
+      put $COMPRESSED_NAME
+      quit
+    EOF
+      if [ $? -eq "1" ] ; then
+        log "ERROR-> during ftp upload"
+        return 1
+      fi
+  fi
+}
+
+function scp_export {
+
+  if [ $SCP_EXPORT == "1" ] ; then
+    scp $SCP_USER@$SCP_HOST:$SCP_PATH
+  fi
+}
   
-log "starting mongodump...." 
 START_TIME=$(date +%s)
 
 prepare_job()
-
-log "$(date) starting mongodump (this will take a while)...." 
-
-cd $BACKUP_PATH
-if [ -d "$BACKUP_PATH/$DO_BACKUP"  ] ; then
-  log "cleaning up previous backup directory..." 
-  rm -rf $BACKUP_PATH/$DO_BACKUP
-fi
-
-$PERFORMANCE_THROTTLING $MONGO_DUMP -h $MONGO_HOST_PORT -d $DO_BACKUP -o $BACKUP_PATH $MONGO_DUMP_OPTIONS
-
-  if [ $? -ne 0 ] ; then
-    log "ERROR -> mongo_dump failed to execute properly: err $?"
-    log "mongosback terminating early" 
-    exit 1
-  fi
-echo -e "$(date) mongodump successful, beginning post-dump operations" 
-
+perform_backup()
+compress()
 archive()
-
+ftp_export()
+scp_export()
 END_TIME=$(date +%s)
 TIME_DIFF=$(( $END_TIME - $START_TIME ))
 TIME_DIFF=`echo $(($TIME_DIFF / 60 ))`
